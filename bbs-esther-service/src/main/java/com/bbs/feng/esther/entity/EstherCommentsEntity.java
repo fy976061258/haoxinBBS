80a09d22a25e20a713a5bc99c8c492fee0ae8075
976061258@qq.com <976061258@qq.com>
2018-03-12 15:59:22 +0800

    更新视频保存

---
+++ bbs-esther-service/src/main/java/com/bbs/feng/esther/entity/EstherCommentsEntity.java
@@ -0,0 +1,11 @@
+package com.bbs.feng.esther.entity;
+
+/**
+ * @author Marco.Feng
+ * @title: haoxinBBS
+ * @description:
+ * @date 2018/03/12 15:58
+ * @since JDK 1.8
+ */
+public class EstherCommentsEntity {
+}
---
+++ bbs-esther-service/src/main/java/com/bbs/feng/esther/entity/EstherDetaileEntity.java
@@ -0,0 +1,32 @@
+package com.bbs.feng.esther.entity;
+
+import lombok.Data;
+import org.apache.catalina.LifecycleState;
+import org.mongodb.morphia.annotations.Entity;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * @author Marco.Feng
+ * @title: haoxinBBS
+ * @description:
+ * @date 2018/03/12 15:48
+ * @since JDK 1.8
+ */
+@Data
+@Entity("EstherDetaile")
+@Document(collection = "EstherDetaile")
+@EnableMongoAuditing
+public class EstherDetaileEntity {
+
+    private String title;
+
+    private String detail;
+
+    private List<EstherImgEntity> estherImgEntities;
+
+    private List<EstherCommentsEntity> estherCommentsEntities;
+
+}
