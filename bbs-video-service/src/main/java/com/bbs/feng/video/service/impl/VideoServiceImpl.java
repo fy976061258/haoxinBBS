f332ad37b45de57934ec2451b680ae8d4b159697
976061258@qq.com <976061258@qq.com>
2018-03-12 09:42:57 +0800

    更新架子

--- bbs-video-service/src/main/java/com/bbs/feng/video/service/impl/VideoServiceImpl.java
+++ bbs-video-service/src/main/java/com/bbs/feng/video/service/impl/VideoServiceImpl.java
@@ -5,6 +5,7 @@ import com.bbs.feng.video.entity.VideoEntity;
 import com.bbs.feng.video.service.VideoService;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import java.util.List;
  * @date 2018/03/12 09:20
  * @since JDK 1.8
  */
+@Service
 public class VideoServiceImpl implements VideoService {
     @Autowired
     private VideoDao videoDao;
