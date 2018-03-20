74475200241fba548549166fd34283b0a181d272
976061258@qq.com <976061258@qq.com>
2018-03-20 14:38:09 +0800

    路由

---
+++ bbs-zuul-service/src/main/java/com/feng/zuul/config/Filter.java
@@ -0,0 +1,44 @@
+package com.feng.zuul.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marco.Feng
+ * @title: haoxinBBS
+ * @description:
+ * @date 2018/03/20 14:31
+ * @since JDK 1.8
+ */
+@Component
+public class Filter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(Filter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String s = String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString());
+        log.info(s);
+        return null;
+    }
+}
