9889421fda7f347ecd97a820df1bd315e6e4698f
976061258@qq.com <976061258@qq.com>
2018-04-02 14:44:06 +0800

    vue2 + iview2 + axios + webpack2

--- bbs-ui-app/src/main.js
+++ bbs-ui-app/src/main.js
@@ -1,15 +1,49 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import routes from './routes'
+import Mock from './mock'
+import iview from 'iview'
+import 'iview/dist/styles/iview.css'
+import VueClipboard from 'vue-clipboard2'
 
-Vue.config.productionTip = false
+Mock.bootstrap();
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+Vue.use(VueClipboard)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+Vue.use(iview)
+
+const router = new VueRouter({
+    routes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({ path: '/login' })
+    } else {
+        next()
+    }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+    el: '#app',
+    template: '<App/>',
+    router,
+    store,
+    components: { App }
+    //render: h => h(Login)
+}).$mount('#app')
+
+//router.replace('/login')
