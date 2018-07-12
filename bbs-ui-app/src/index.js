384b777d66f264a015bc9308d92d2f08d9c485c5
976061258@qq.com <976061258@qq.com>
2018-07-12 21:48:48 +0800

    简单修改

--- bbs-ui-app/src/index.js
+++ bbs-ui-app/src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
-import {Provider} from 'react-redux';
-import store from './redux/store';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from 'components/App/App';
+import ReactDOM from 'react-dom';
+import App from './index/App';
+import registerServiceWorker from './registerServiceWorker';
 
-renderWithHotReload(App);
 
-if (module.hot) {
-    module.hot.accept('components/App/App', () => {
-        const NextApp = require('components/App/App').default;
-        renderWithHotReload(NextApp);
-    });
-}
-
-function renderWithHotReload(RootElement) {
-    ReactDom.render(
-        <AppContainer>
-            <Provider store={store}>
-                <Router>
-                    <RootElement/>
-                </Router>
-            </Provider>
-        </AppContainer>,
-        document.getElementById('app')
-    )
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
